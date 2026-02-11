d16deb72157b604f74e8e9069b117e2daf056167
Sarthak <sarthak.debnath06@gmail.com>
2026-02-11 17:52:51 +0530

    Main BackEnd struct

---
+++ back_end/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Digital Journal Backend Running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
